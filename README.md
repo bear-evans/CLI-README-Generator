cd5a9d0db696ad2a344e98bd7cd902d7695a369d
bear-evans <bear-evans@outlook.com>
2021-06-23 12:12:44 -0400

    Wrote template literal

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# CLI-README-Generator
-A README generator run in the command line, powered by and Inquirer.
+# Bear's README Generator
+
+## Description
+
+Bear's README Generator is a command-line application that generates README files based on user input. In fact, this README was created using this program.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+
+## Installation
+
+To run this project, you must install node.js and inquirer. To install node.js, go to the [official Node.js website](https://nodejs.org/) and download the installer. To install inquirer, run 'npm install inquirer' from the terminal.
+
+## Usage
+
+Run the program using the command 'node index.js'. Follow the prompts given. Note that due to limitations of inquirer, multi-line input is not supported.
+
+## Contribution
+
+Bear's README Generator is open source and free to use or modify. There is no requirement to credit. Contributors are encouraged to follow the fork-and-pull method and to merge changes with the main branch before submitting a pull request.
+
+## Tests
+
+There are currently no specific testing procedures to run.
+
+## License
+
+This project is licensed under the terms of the MIT license.
--- index.js
+++ index.js
@@ -1,2 +1,103 @@
+/*==========================================
+Requires
+===========================================*/
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+/*==========================================
+Greeting Message
+===========================================*/
+
+console.log("=============================================");
+console.log("Welcome to Bear's README Generator!          ");
+console.log("---------------------------------------------");
+console.log("Just answer some basic questions, and your   ");
+console.log("README file will be semi-automatically       ");
+console.log("generated and saved to a ready-to-go file!   ");
+console.log("=============================================");
+
+/*==========================================
+Inquirer Prompt for User Data
+===========================================*/
+
+inquirer
+  .prompt([
+    {
+      name: "projectName",
+      type: "input",
+      message: "What is the name of your project?",
+    },
+    {
+      name: "description",
+      type: "input",
+      message: "Please describe your project.",
+    },
+    {
+      name: "install",
+      type: "input",
+      message: "How do you install this project?",
+    },
+    {
+      name: "usage",
+      type: "input",
+      message: "How do you use this project?",
+    },
+    {
+      name: "contribution",
+      type: "input",
+      message: "What are your guidelines for contribution?",
+    },
+    {
+      name: "tests",
+      type: "input",
+      message: "What are the instructions for running tests?",
+    },
+    {
+      name: "license",
+      type: "list",
+      message: "What license will you release under?",
+      choices: [
+        "MIT",
+        "Creative Commons Attribution Share Alike 4.0",
+        "GNU General Public v3.0",
+        "Mozilla Public 2.0",
+      ],
+    },
+  ])
+  .then((data) => {
+    let fileContents = `# ${data.projectName}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## Contribution
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## License
+
+This project is licensed under the terms of the ${data.license} license.`;
+
+    fs.writeFile(data.projectName + "README.md", fileContents, (err) => {
+      err ? console.log(err) : console.log("README generated!");
+    });
+  });
