1b4b6bdff2d50217bef45332c6e09d9f35600e24
bear-evans <bear-evans@outlook.com>
2021-06-24 20:26:26 -0400

    Removed redundant code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("./utils/generateMarkdown.js");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -27,7 +27,30 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  output = "";
+
+  switch (license) {
+    case "MIT":
+      output =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "Creative Commons Attribution Share Alike 4.0":
+      output =
+        "[![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-sa/4.0/)";
+      break;
+    case "GNU General Public v3.0":
+      output =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "Mozilla Public 2.0":
+      output =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+  }
+
+  return output;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
